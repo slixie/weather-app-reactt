ed8661da105c87172eeaa1f9e7c7fb571998659c
slixie <111938433+slixie@users.noreply.github.com>
2022-12-29 23:43:27 +0200

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="Container">
-        <Weather />
+        <Weather defaultCity="Kyiv" />
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,143 +1,163 @@
-import React from "react";
-import "./Weather.css";
+import React, { useState } from "react";
 import axios from "axios";
+import "./Weather.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Kyiv",
-    date: "Friday 07:47",
-    temperature: 8,
-    description: "Few Clouds",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-    humidity: 69,
-    wind: 2,
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-  return (
-    <div className="Weather">
-      <div className="page">
-        <div className="col-12">
-          <p className="heading-item">{weatherData.date}</p>
-        </div>
-        <form className="typeCity">
-          <input
-            className="search-text"
-            type="text"
-            placeholder="Enter your city"
-            autoComplete="off"
-          />
-          <button className="search-btn">
-            <i className="fa-solid fa-magnifying-glass"></i>
-          </button>
-          <button className="current">
-            <i className="fa-solid fa-location-dot"></i>
-          </button>
-        </form>
+  function hadleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      date: "Friday 07:47",
+      city: response.data.name,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      feels_like: response.data.main.feels_like,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      temp_max: response.data.main.temp_max,
+      temp_min: response.data.main.temp_min,
+    });
+  }
 
-        <div className="row heading">
-          <div className="col-6 city-name">
-            <i className="fa-solid fa-location-dot icon_current_location"></i>
-            <p className="cityName">{weatherData.city}</p>
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="page">
+          <div className="col-12">
+            <p className="heading-item">{weatherData.date}</p>
           </div>
-        </div>
-        <div className="col-12">
-          <p className="currentWeather">Current weather</p>{" "}
-        </div>
-        <div className="row mainPage">
-          <div className="col-6 center">
-            <img
-              src={weatherData.imgUrl}
-              alt="icon"
-              className="main_icon"
-              width="85px"
+          <form className="typeCity">
+            <input
+              className="search-text"
+              type="text"
+              placeholder="Enter your city"
+              autoComplete="off"
             />
-            <p className="city-temperature">{weatherData.temperature}</p>
-            <span className="units">
-              <a href="/" className="celsius-link">
-                °C
-              </a>{" "}
-              |
-              <a href="/" className="fahrenheit-link">
-                °F
-              </a>
-            </span>
+            <button className="search-btn">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+            <button className="current">
+              <i className="fa-solid fa-location-dot"></i>
+            </button>
+          </form>
+
+          <div className="row heading">
+            <div className="col-6 city-name">
+              <i className="fa-solid fa-location-dot icon_current_location"></i>
+              <p className="cityName">{weatherData.city}</p>
+            </div>
           </div>
-          <div className="col-6 weather_description center">
-            <h4>
-              <span>{weatherData.description}</span>
-            </h4>
-            <h5>Feels like: 7 °C</h5>
+          <div className="col-12">
+            <p className="currentWeather">Current weather</p>{" "}
+          </div>
+          <div className="row mainPage">
+            <div className="col-6 center">
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="main_icon"
+                width="85px"
+              />
+              <p className="city-temperature">
+                {Math.round(weatherData.temperature)}
+              </p>
+              <span className="units">
+                <a href="/" className="celsius-link">
+                  °C
+                </a>{" "}
+                |
+                <a href="/" className="fahrenheit-link">
+                  °F
+                </a>
+              </span>
+            </div>
+            <div className="col-6 weather_description center">
+              <h4>
+                <span>{weatherData.description}</span>
+              </h4>
+              <h5>Feels like: {Math.round(weatherData.feels_like)} °C</h5>
+            </div>
+
+            <div className="col-3 statistics">
+              <div className="statistic"> WIND </div>
+              {Math.round(weatherData.wind)} km/h
+            </div>
+            <div className="col-3 statistics">
+              <div className="statistic"> HUMIDITY </div>
+              {weatherData.humidity} %
+            </div>
+            <div className="col-3 statistics">
+              <div className="statistic"> TEMP MAX </div>
+              {Math.round(weatherData.temp_max)} °C
+            </div>
+            <div className="col-3 statistics">
+              <div className="statistic"> TEMP MIN </div>
+              {Math.round(weatherData.temp_min)} °C
+            </div>
           </div>
 
-          <div className="col-3 statistics">
-            <div className="statistic"> WIND </div>
-            {weatherData.wind} km/h
+          <div className="col-12">
+            <p className="forecast">Weather forecast</p>
           </div>
-          <div className="col-3 statistics">
-            <div className="statistic"> HUMIDITY </div>
-            {weatherData.humidity} %
+
+          <div className="weather_forecast" id="weather_forecast">
+            {" "}
+            <div className="row forecastDays">
+              <div className="col-2 days">
+                Mon <br />
+                <i className="fa-solid fa-sun forecastIcon"></i>
+                <br /> 20°/24°
+              </div>
+              <div className="col-2 days">
+                Tue <br />
+                <i className="fa-solid fa-sun forecastIcon"></i>
+                <br /> 19°/22°
+              </div>
+              <div className="col-2 days">
+                Wed <br />
+                <i className="fa-solid fa-cloud forecastIcon"></i>
+                <br /> 17°/20°
+              </div>
+              <div className="col-2 days">
+                Thu <br />
+                <i className="fa-solid fa-cloud-bolt forecastIcon"></i>
+                <br /> 15°/17°
+              </div>
+              <div className="col-2 days">
+                Fri <br />
+                <i className="fa-solid fa-bolt-lightning forecastIcon"></i>
+                <br /> 17°/20°
+              </div>
+              <div className="col-2 days">
+                Sat <br />
+                <i className="fa-solid fa-cloud forecastIcon"></i>
+                <br /> 17°/20°
+              </div>
+            </div>
           </div>
-          <div className="col-3 statistics">
-            <div className="statistic"> TEMP MAX </div>
-            15 °C
-          </div>
-          <div className="col-3 statistics">
-            <div className="statistic"> TEMP MIN </div>2 °C
-          </div>
+
+          <p className="credit">
+            <a
+              href="https://github.com/slixie/weather-app-reactt"
+              className="open-source-code"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Coded
+            </a>{" "}
+            by Mariia Sytnykkk
+          </p>
         </div>
-
-        <div className="col-12">
-          <p className="forecast">Weather forecast</p>
-        </div>
-
-        <div className="weather_forecast" id="weather_forecast">
-          {" "}
-          <div className="row forecastDays">
-            <div className="col-2 days">
-              Mon <br />
-              <i className="fa-solid fa-sun forecastIcon"></i>
-              <br /> 20°/24°
-            </div>
-            <div className="col-2 days">
-              Tue <br />
-              <i className="fa-solid fa-sun forecastIcon"></i>
-              <br /> 19°/22°
-            </div>
-            <div className="col-2 days">
-              Wed <br />
-              <i className="fa-solid fa-cloud forecastIcon"></i>
-              <br /> 17°/20°
-            </div>
-            <div className="col-2 days">
-              Thu <br />
-              <i className="fa-solid fa-cloud-bolt forecastIcon"></i>
-              <br /> 15°/17°
-            </div>
-            <div className="col-2 days">
-              Fri <br />
-              <i className="fa-solid fa-bolt-lightning forecastIcon"></i>
-              <br /> 17°/20°
-            </div>
-            <div className="col-2 days">
-              Sat <br />
-              <i className="fa-solid fa-cloud forecastIcon"></i>
-              <br /> 17°/20°
-            </div>
-          </div>
-        </div>
-
-        <p className="credit">
-          <a
-            href="https://github.com/slixie/weather-app-reactt"
-            className="open-source-code"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Coded
-          </a>{" "}
-          by Mariia Sytnykkk
-        </p>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "fc5e25b8c524cb4b4ebd4ca86a442c09";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(hadleResponse);
+
+    return "Loading...";
+  }
 }
